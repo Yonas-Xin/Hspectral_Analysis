f9a5f72b962a0e107d1a6cfdb0d6cad4c3bac661
Yonas <850021443@qq.com>
2025-06-28 23:07:14 +0800

    Additionally create an  "report.py" file to print the classification report

---
+++ cnn_model/report.py
@@ -0,0 +1,69 @@
+import sys, os
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base_path)
+import torch
+import torch.optim as optim
+from cnn_model.Models.Models import Constrastive_learning_Model, Shallow_3DCNN, Shallow_1DCNN
+from cnn_model.Models.Data import Dataset_3D, Dataset_1D
+from torch.optim.lr_scheduler import StepLR,ExponentialLR,ReduceLROnPlateau
+from cnn_model.Models.Frame import Cnn_model_frame
+from utils import read_txt_to_list, rewrite_paths_info
+from torch.utils.data import DataLoader
+from multiprocessing import cpu_count
+import math
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, cohen_kappa_score
+import numpy as np
+
+def print_result_report(model, eval_dataloader, log_writer, device):
+    model.to(device)
+    model.eval()
+    all_labels = []
+    all_preds = []
+    with torch.no_grad():
+        for data, label in tqdm(eval_dataloader, desc='Generating Report', total=len(eval_dataloader)):
+            data, label = data.to(device), label.to(device)
+            output = model(data)
+            _, preds = torch.max(output, 1)
+            all_labels.extend(label.cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+
+        accuracy = accuracy_score(all_labels, all_preds)
+        clf_report = classification_report(all_labels, all_preds, digits=4)
+        conf_matrix = confusion_matrix(all_labels, all_preds)
+        kappa = cohen_kappa_score(all_labels, all_preds)
+
+        log_writer.write(f"\n\nTest_acc: {accuracy:.4f}\n")
+        log_writer.write(f"Cohen's Kappa: {kappa:.4f}\n")
+        log_writer.write(f"Classification Report:\n")
+        log_writer.write(clf_report + "\n\n")
+        log_writer.write("Confusion Matrix:\n")
+        log_writer.write(np.array2string(conf_matrix, separator=', '))
+        log_writer.write('\n')
+        log_writer.flush()
+
+        print("Test Accuracy:", accuracy)
+        print(f"Cohen's Kappa: {kappa:.4f}")
+        print("Classification Report:\n", clf_report)
+        print("Confusion Matrix:\n", conf_matrix)
+if __name__ == '__main__':
+    writer_name = '1DCNN.log'
+    saved_model_name = r'D:\Programing\pythonProject\Hspectral_Analysis\cnn_model\_results\models_pth\SSAR_202506261846.pth'
+    batch = 36 # batch
+    test_images_dir = r'd:\Data\Hgy\龚鑫涛试验数据\program_data\handle_class\clip_data_15classs_1x1\Aeval_datasets.txt'  # 测试数据集
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # 显卡设置
+    out_classes = 15 # 分类数
+
+
+    out_embeddings = 24 # 模型初始化必要，后面打算把这个参数设置为固定值
+    dataloader_num_workers = cpu_count() // 4 # 根据cpu核心数自动决定num_workers数量
+    print(f'Using num_workers: {dataloader_num_workers}')
+    # 配置训练数据集和模型
+    test_image_lists = rewrite_paths_info(test_images_dir)
+    eval_dataset = Dataset_1D(test_image_lists)
+    model = Shallow_1DCNN(out_embedding=out_embeddings, out_classes=out_classes, in_shape=eval_dataset.data_shape)  # 模型实例化
+    model.load_state_dict(torch.load(saved_model_name, weights_only=True, map_location=device)['model'])
+    eval_dataloader = DataLoader(eval_dataset, batch_size=batch, shuffle=True, pin_memory=True, 
+                                num_workers=dataloader_num_workers, prefetch_factor=2,persistent_workers=True)  # 数据迭代器
+    log_writer = open(writer_name, 'w')
+    print_result_report(model=model, eval_dataloader=eval_dataloader, log_writer=log_writer, device=device)
