e0fdecc6564e3d14124d6ec2e5064e321e5e950a
Yonas <850021443@qq.com>
2025-07-22 16:52:40 +0800

    Moco frame update

--- contrastive_learning/Models/Moco_Frame.py
+++ contrastive_learning/Models/Moco_Frame.py
@@ -5,6 +5,7 @@ import signal
 import shutil
 from pathlib import Path
 from datetime import datetime
+import time
 import torch.nn as nn
 from tqdm import tqdm
 from multiprocessing import cpu_count
@@ -14,15 +15,13 @@ import traceback
 # from contrastive_learning.Models.loss import InfoNCELoss
 from utils import save_matrix_to_csv
 
-class Contrastive_learning_frame:
-    def __init__(self, augment, model_name, min_lr=1e-7, epochs=300, warmup_epochs=30, device=None, if_full_cpu=True):
+class Moco_Frame:
+    '''配置训练参数与模型保存地址等'''
+    def __init__(self, augment, model_name, min_lr=1e-7, epochs=300, device=None, if_full_cpu=True):
         self.augment = augment
-        self.infonce = nn.CrossEntropyLoss()
+        self.loss = nn.CrossEntropyLoss()
         self.min_lr = min_lr
         self.epochs=epochs
-        if warmup_epochs is not None:
-            self.warmup_epochs = warmup_epochs
-        else: self.warmup_epochs = 0
 
         if device is None:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -43,45 +42,11 @@ class Contrastive_learning_frame:
         self.model_path = os.path.join(model_dir, f'{model_save_name}.pth')
         self.log_path = os.path.join(log_dir, f'{model_save_name}.log')
         self.tensorboard_dir = os.path.join(self.parent_dir, f'tensorboard_logs\\logs_{model_save_name}')
-        self.log_writer = None
-        self.writer = None
 
         #配置训练信息
         self.if_full_cpu = if_full_cpu
         self.train_epoch_min_loss = 100
         self.start_epoch = 0
-
-    def clean_up(self):
-        """清理日志文件和tensorboard目录"""
-        if not os.path.exists(self.model_path):
-            if os.path.exists(self.log_path):
-                self.log_writer.close()
-                os.remove(self.log_path)
-                print(f"Removed log file: {self.log_path}")
-            if os.path.exists(self.tensorboard_dir):
-                self.writer.close()
-                shutil.rmtree(self.tensorboard_dir)
-                print(f"Removed tensorboard directory: {self.tensorboard_dir}")
-        else: pass
-    def load_parameter(self, model, optimizer, scheduler=None, ck_pth=None, load_from_ck=False): # 加载模型、优化器、调度器
-        self.full_cpu() # 打印配置信息
-        if ck_pth is not None:
-            checkpoint = torch.load(ck_pth, weights_only=True, map_location=self.device)  # 加载断点
-            model.load_state_dict(checkpoint['model'])
-            if load_from_ck:
-                self.train_epoch_min_loss = checkpoint.get('best_loss', 100)
-                try:
-                    optimizer.load_state_dict(checkpoint['optimizer']) # 恢复优化器
-                    print('The optimizer state have been loaded!')
-                    self.start_epoch = checkpoint.get('epoch', -1) + 1  # 获取epoch信息，如果没有，默认为0
-                except(ValueError, RuntimeError):
-                    print('The optimizer is incompatible, and the parameters do not match')
-                if scheduler and 'scheduler' in checkpoint: # 恢复调度器
-                    try:
-                        scheduler.load_state_dict(checkpoint['scheduler'])
-                    except (ValueError, RuntimeError):
-                        print('The scheduler is incompatible')
-            print(f"Loaded checkpoint from epoch {self.start_epoch}, current lr {optimizer.param_groups[0]['lr']}")
 
     def full_cpu(self):
         cpu_num = cpu_count()  # 自动获取最大核心数目
@@ -93,79 +58,129 @@ class Contrastive_learning_frame:
         if self.if_full_cpu:
             torch.set_num_threads(cpu_num)
             print('Using cpu core num: ', cpu_num)
-        print('Is cuda availabel: ', torch.cuda.is_available())  # 是否支持cuda
-        print('Cuda device count: ', torch.cuda.device_count())  # 显卡数
-        print('Current device: ', torch.cuda.current_device())  # 当前计算的显卡id
+        print(f'Cuda device count: {torch.cuda.device_count()} And the current device:{self.device}, Device ID: {torch.cuda.current_device()}')  # 显卡数
 
-    def save_model(self, model, optimizer, scheduler, epoch=None, avg_loss=None):
-        state = {
-            'model': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'epoch': epoch,
-            'best_loss': avg_loss,
-            'scheduler': scheduler.state_dict() if scheduler else None,
-            'current_lr': optimizer.param_groups[0]['lr']
-        }
-        torch.save(state, self.model_path)
-        print(f"============Checkpoint saved at epoch {epoch + 1}============")
+def clean_up(frame,log_writer,tensor_writer):
+    """清理日志文件和tensorboard目录"""
+    if not os.path.exists(frame.model_path):
+        if os.path.exists(frame.log_path):
+            log_writer.close()
+            os.remove(frame.log_path)
+            print(f"Removed log file: {frame.log_path}")
+        if os.path.exists(frame.tensorboard_dir):
+            tensor_writer.close()
+            shutil.rmtree(frame.tensorboard_dir)
+            print(f"Removed tensorboard directory: {frame.tensorboard_dir}")
+        else: pass
 
-    def train(self, model, optimizer, dataloader, scheduler=None, ck_pth=None, clean_noise_samples=False, load_from_ck=False, clean_th=0.99):
-        self.log_writer = open(self.log_path, 'w')
-        if not os.path.exists(self.tensorboard_dir):
-            os.makedirs(self.tensorboard_dir)
-        self.writer = SummaryWriter(log_dir=self.tensorboard_dir)
-        model.to(self.device)
-        self.load_parameter(model=model, optimizer=optimizer, scheduler=scheduler, ck_pth=ck_pth, load_from_ck=load_from_ck) # 初始化模型
-        model.train() # 开启训练模式，自训练没有测试模式，所以这个可以在训练之前设置
-        
-        model_save_epoch = 0
-        try:
-            for epoch in range(self.start_epoch, self.epochs):
-                running_loss = 0.0
-                for block in tqdm(dataloader, total=len(dataloader), desc="Train", leave=True):
-                    block = block.to(self.device) # batch,C,H,W
-                    with torch.no_grad():
-                        q = self.augment(block)
-                        k = self.augment(block)
-                    optimizer.zero_grad()  # 清空梯度
-                    logits, label = model(q, k)
+def save_model(frame, model, optimizer, scheduler, epoch=None, avg_loss=None, avg_acc=None):
+    state = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'epoch': epoch,
+        'best_loss': avg_loss,
+        'best_acc': avg_acc,
+        'scheduler': scheduler.state_dict() if scheduler else None,
+        'current_lr': optimizer.param_groups[0]['lr']
+    }
+    torch.save(state, frame.model_path)
+    print(f"============Checkpoint saved at epoch {epoch + 1}============")
 
-                    loss = self.infonce(logits, label)  # 对比损失
-                    loss.backward()  # 反向传播
-                    optimizer.step()  # 更新权重
-                    running_loss += loss.item()
-                avg_loss = running_loss / len(dataloader)
-                current_lr = optimizer.param_groups[0]['lr']
-                result = f"Epoch-{epoch + 1} , Loss: {avg_loss:.8f}, Lr: {current_lr:.8f}"
-                self.log_writer.write(result + '\n') # 记录训练过程
-                self.writer.add_scalar('Train/Loss', avg_loss, epoch) # 记录到tensorboard
-                print(result)
-                if avg_loss >= self.train_epoch_min_loss:  # 若当前epoch的loss大于等于之前最小的loss
-                    pass
-                else:
-                    self.train_epoch_min_loss = avg_loss
-                    model_save_epoch = epoch
-                    self.save_model(model=model, optimizer=optimizer, scheduler=scheduler, epoch=epoch, avg_loss=avg_loss)
-                if (epoch + 1) < self.warmup_epochs or current_lr <= self.min_lr:
-                    pass
-                else:
-                    scheduler.step()
-                self.log_writer.flush()
-            result = f'Model saved at Epoch{model_save_epoch}. \nThe best training_loss:{self.train_epoch_min_loss}'
-            self.log_writer.write(result + '\n')
-            print(result)
-        except KeyboardInterrupt: # 捕获键盘中断信号
-            print(f"Training interrupted due to: KeyboardInterrupt")
-            self.clean_up()
-        except Exception as e: 
-            print(traceback.format_exc())  # 打印完整的堆栈跟踪
-            self.clean_up()
-        finally:
-            self.log_writer.close()
-            self.writer.close()
-            print("Training completed. Program exited.")
-            sys.exit(0)
+def load_parameter(frame, model, optimizer, scheduler=None, ck_pth=None, load_from_ck=False): # 加载模型、优化器、调度器
+    frame.full_cpu() # 打印配置信息
+    if ck_pth is not None:
+        if load_from_ck:
+            checkpoint = torch.load(ck_pth, weights_only=True, map_location=frame.device)  # 加载断点
+            model.load_state_dict(checkpoint['model'])
+            frame.train_epoch_min_loss = checkpoint.get('best_loss', 100)
+            try:
+                optimizer.load_state_dict(checkpoint['optimizer']) # 恢复优化器
+                print('The optimizer state have been loaded!')
+                frame.start_epoch = checkpoint.get('epoch', -1) + 1  # 获取epoch信息，如果没有，默认为0
+            except(ValueError, RuntimeError):
+                print('The optimizer is incompatible, and the parameters do not match')
+            if scheduler and 'scheduler' in checkpoint: # 恢复调度器
+                try:
+                    scheduler.load_state_dict(checkpoint['scheduler'])
+                except (ValueError, RuntimeError):
+                    print('The scheduler is incompatible')
+            print(f"Loaded checkpoint from epoch {frame.start_epoch}, current lr {optimizer.param_groups[0]['lr']}")
+
+def train(frame:Moco_Frame, model, optimizer, dataloader, scheduler=None, ck_pth=None, load_from_ck=False):
+    start_time = time.time()
+    log_writer = open(frame.log_path, 'w')
+    if not os.path.exists(frame.tensorboard_dir):
+        os.makedirs(frame.tensorboard_dir)
+    tensor_writer = SummaryWriter(log_dir=frame.tensorboard_dir)
+    model.to(frame.device)
+    load_parameter(frame=frame, model=model, optimizer=optimizer, scheduler=scheduler, ck_pth=ck_pth, load_from_ck=load_from_ck) # 初始化模型
+    model.train() # 开启训练模式，自训练没有测试模式，所以这个可以在训练之前设置
     
+    model_save_epoch = 0
+    max_iter_num = len(dataloader)
+    interval_printinfo = max_iter_num // 10 # 隔10次打印一次loss
+
+    # start training
+    try:
+        for epoch in range(frame.start_epoch, frame.epochs):
+            running_loss = 0.0
+            for i,block in tqdm(enumerate(dataloader), total=len(dataloader), desc="Train", leave=True):
+                block = block.to(frame.device) # batch,C,H,W
+                with torch.no_grad():
+                    q = frame.augment(block)
+                    k = frame.augment(block)
+                optimizer.zero_grad()  # 清空梯度
+                logits, label = model(q, k)
+
+                loss = frame.loss(logits, label)
+                loss.backward()  # 反向传播
+                optimizer.step()  # 更新权重
+                running_loss += loss.item()
+                if (i+1) % interval_printinfo == 0:
+                    print(f"\nStep: {i}, The current loss: {loss:.6f}, The Avgloss: {running_loss/(i+1):.6f}")
+            avg_loss = running_loss / len(dataloader)
+            current_lr = optimizer.param_groups[0]['lr']
+            result = f"Epoch-{epoch + 1} , Loss: {avg_loss:.8f}, Lr: {current_lr:.8f}"
+            log_writer.write(result + '\n') # 记录训练过程
+            tensor_writer.add_scalar('Train/Loss', avg_loss, epoch) # 记录到tensorboard
+            print(result)
+            if avg_loss >= frame.train_epoch_min_loss:  # 若当前epoch的loss大于等于之前最小的loss
+                pass
+            else:
+                frame.train_epoch_min_loss = avg_loss
+                model_save_epoch = epoch
+                save_model(frame=frame, model=model, optimizer=optimizer, scheduler=scheduler, epoch=epoch, avg_loss=avg_loss)
+            if current_lr <= frame.min_lr:
+                pass
+            else:
+                scheduler.step()
+            log_writer.flush()
+        
+        # 打印和记录结果
+        result = f'Model saved at Epoch{model_save_epoch}. \nThe best training_loss:{frame.train_epoch_min_loss}'
+        end_time = time.time()
+        total_seconds = end_time - start_time
+        hours = int(total_seconds // 3600)
+        minutes = int((total_seconds % 3600) // 60)
+        seconds = int(total_seconds % 60)
+        runtime = f'Program runtime: {hours}h {minutes}m {seconds}s'
+        log_writer.write(result + '\n')
+        log_writer.write(runtime + '\n')
+        print(result)
+        print(runtime)
+    except KeyboardInterrupt: # 捕获键盘中断信号
+        print(f"Training interrupted due to: KeyboardInterrupt")
+        clean_up(frame=frame, log_writer=log_writer, tensor_writer=tensor_writer)
+    except Exception as e: 
+        print(traceback.format_exc())  # 打印完整的堆栈跟踪
+        clean_up(frame=frame, log_writer=log_writer, tensor_writer=tensor_writer)
+    finally:
+        log_writer.close()
+        tensor_writer.close()
+        print(f"Training completed. Program exited.")
+        sys.exit(0)
+
+
 class Contrasive_learning_predict_frame:
     def __init__(self, device=None):
         if device is None:
--- contrastive_learning/Models/Models.py
+++ contrastive_learning/Models/Models.py
@@ -21,10 +21,24 @@ class Spe_Spa_Attenres(nn.Module):
     def predict(self, x):
         return self.encoder(x)
 
+class Contra_Res18(nn.Module):
+    def __init__(self, out_embedding=1024, in_shape=(138,17,17)):
+        super().__init__()
+        self.encoder = ResNet_3D(block=Basic_Residual_block, layers=[2,2,2,2], num_classes=out_embedding) # 3d卷积残差编码器
+        self.decoder = Spe_Spa_Atten_Decoder(out_embedding, 128, mid_channels=128)
+    def forward(self, x):
+        if x.dim() == 4:
+            x = x.unsqueeze(1)  # 增加一个维度到 [B, 1, C, H, W]
+        elif x.dim() != 5:
+            raise ValueError(f"Expected input dimension 4 or 5, but got {x.dim()}")
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
 class Moco3D(nn.Module): # 单GPU训练的Moco框架
     def __init__(self, 
-                 base_model: nn.Module = Res_3D_18Net, 
-                 out_embedding: int = 24, 
+                 base_model: nn.Module = Contra_Res18, 
+                 out_embedding: int = 1024, 
                  in_shape: tuple = (138, 17, 17),
                  dim: int = 128,
                  K: int = 65536,
@@ -35,8 +49,8 @@ class Moco3D(nn.Module): # 单GPU训练的Moco框架
         self.T = T
         self.m = m
 
-        self.encoder_q = base_model(out_classes=128, in_shape=in_shape)
-        self.encoder_k = base_model(out_classes=128, in_shape=in_shape)
+        self.encoder_q = base_model(out_embedding=out_embedding, in_shape=in_shape)
+        self.encoder_k = base_model(out_embedding=out_embedding, in_shape=in_shape)
         for param_q, param_k in zip(
             self.encoder_q.parameters(), self.encoder_k.parameters()
         ):
